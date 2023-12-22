d2511a2b40f7042882464912242bd7258f394361
abishekguru <152874982+abishekguru@users.noreply.github.com>
2023-12-22 14:12:22 +0530

    Added a c program and cpp program

---
+++ namesort.cpp
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  int n;
+  printf("enter number of names");
+  scanf("%d",&n);
+  if(n>>3)
+    exit(0);
+  char a[n][25];
+  char t[25];
+  printf("Enter the names");
+  for(int i=0;i<n;i++)
+   cin>>a[i];
+  printf("Original list\n");
+  for(int i=0;i<n;i++)
+   cout<<a[i]<<" ";
+   cout<<endl;
+  for(int i=0;i<n;i++)
+  {
+     for(int j=i+1;j<n;j++)
+     {
+       if(strcmp(a[j],a[i])<0)
+        {
+           strcpy(t,a[j]);
+           strcpy(a[j],a[i]);
+           strcpy(a[i],t);
+        }
+     }
+  }
+  printf("Sorted name list:\n");
+  for(int i=0;i<n;i++)
+   cout<<a[i]<<" ";
+  cout<<endl;
+}
+
